2f145807e0bb86d478c572d783a8825cccaf938d
Humberto Gonzalez <humberto.gonzalezj@gmail.com>
2020-06-08 15:42:58 -0700

    Adding in files for assignment

--- main.py
+++ main.py
@@ -6,20 +6,45 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+post_url  = "http://hidden-journey-62459.herokuapp.com/piglatinize/"
+base_url = "http://hidden-journey-62459.herokuapp.com/"
+
 
 def get_fact():
-
     response = requests.get("http://unkno.com")
 
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
-    return facts[0].getText()
+    return facts[0].getText().strip()
+
+
+def get_pig_latin_response(text):
+    request = requests.post(post_url,
+                            data = {'input_text':text},
+                            allow_redirects=False)
+    
+    return request
+
+def extract_url(response):
+    url = response.headers['location']
+     
+    return url
+
+
+def get_pig_latin_url(text):
+    response = get_pig_latin_response(text)
+    url = extract_url(response)
+    
+    return url
+    
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    random_fact = get_fact()
+    url = get_pig_latin_url(random_fact)
+    return url
 
 
 if __name__ == "__main__":
